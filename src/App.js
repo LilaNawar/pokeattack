873418e7eb79a63944f2d50f687631ec808ae7e4
Lila <lila.nawar21@gmail.com>
2022-04-14 11:19:40 -0400

    used starter code and made component based pokemon atttack

--- src/App.js
+++ src/App.js
@@ -4,9 +4,26 @@ import React, { Component } from 'react'
 import Pokemon from './components/Pokemon/Pokemon';
 
 export default class App extends Component {
+  state ={
+    hp1:120,
+    hp2:300
+  }
+  handlePikattack =() =>{
+    if (this.state.hp1 >0){
+      this.setState({hp1:this.state.hp1 -10})
+    }
+  }
+  handleCharattack =() =>{
+    if (this.state.hp2 >0){
+    this.setState({hp2:this.state.hp2 -10})
+  }
+}
   render() {
     return (
-      <div className="App">App</div>
+    <div className="App">
+        <Pokemon name={'Ditto'} hp={this.state.hp1} image={'https://pokestop.io/img/pokemon/charizard-256x256.png'} handleattack={this.handleCharattack}/>
+        <Pokemon name={'jerry'} hp={this.state.hp2} image={'https://pokestop.io/img/pokemon/pikachu-256x256.png'} handleattack={this.handlePikattack}/>
+    </div>
     )
   }
 }
--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Pokemon(props) {
   return (
-    <div className="Pokemon">Pokemon</div>
+    <div className="Pokemon">
+    <h1>{props.name} HP:{props.hp}</h1>
+    <img src={props.image} />
+    <button onClick= {props.handleattack}>Attack</button>
+    </div>
   )
 }
